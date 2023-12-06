4986aa341be1b5b34f03afa38ec26288f667007f
David Zamora <zdavid505@gmial.com>
2023-12-05 22:29:33 -0600

    Se añadio el process

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 //Asignacion de puerto en expresss
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //Conexion a base de datos
 const db = mysql.createConnection({
